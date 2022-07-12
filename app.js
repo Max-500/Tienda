ecae33331e92ff08c7314f199d73e29cd8532f6a
Max-500 <213497@ids.upchiapas.edu.mx>
2022-07-12 09:15:01 -0500

    Cambios: Metodo Post a Get y Fetch; Funcional || Login - Nombre

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(session({
 const connection = require('./BDD/bdd');
 const { name } = require('ejs');
 
+// Rutas para VISTAS INICIO ----------------------------------------------------------------
 app.get('/', async (req, res) => {
     res.render(__dirname + '/vistas/inicio')
 })
@@ -41,6 +42,11 @@ app.get('/menu', async (req, res) => {
     res.render(__dirname + '/vistas/menu')
 })
 
+// Rutas para VISTAS FIN ----------------------------------------------------------------
+
+
+// Rutas para APIS INICIO ----------------------------------------------------------------
+/*
 app.post('/login', async (req, res) => {
     const name = req.body.user
     const matricula = req.body.matricula
@@ -52,25 +58,37 @@ app.post('/login', async (req, res) => {
             res.render(__dirname + '/vistas/login')
     })
 })
+*/
+app.get('/menu_api', (req, res) => {
+    const matricula = req.query.matricula
 
-app.post('/menu', (req, res) => {
-    let arreglo = new Array;
-    const nombre = req.body.nombre
-    const matricula = req.body.matricula
-    const password = req.body.password
-    //const queryUpdate = "UPDATE `usuario` SET dinero=" + dineroF + ' WHERE name= "' + name + '"';
-    connection.query('SELECT * FROM usuarios WHERE matricula = ?', matricula, async (error, results) => {
-        arreglo = results
-        console.log(arreglo)
-        if(arreglo.length == []){
-            res.render(__dirname + '/vistas/login')
-        }else if(arreglo.length == 1 && arreglo[0].nombre == nombre && arreglo[0].contrasena == password){
-            res.render(__dirname + '/vistas/menu')
-        }else{
-            res.render(__dirname + '/vistas/login')
+    connection.query(`SELECT * FROM usuarios WHERE matricula = ${matricula} `, async (error, results) => {
+        console.log()
+        if (results.length > 0) {
+            res.send({ redirect: results })
+        } else {
+            res.send({ redirect: {"results": ""} });
         }
     })
-})
+});
+
+app.get('/login_api', (req, res) => {
+    const nombre = req.query.nombre
+    const matricula = req.query.matricula
+    const password = req.query.password
+    // && matricula = ${matricula} && contrasena = ${password}"
+    connection.query(`SELECT * FROM usuarios WHERE nombre = "${nombre}";`, async (error, results) => {
+        console.log('Entro')
+        //console.log(results[0])
+        if (results.length > 0) {
+            res.send({ redirect: results })
+        } else {
+            res.send({ redirect: {"results": ""} });
+        }
+    })
+});
+
+// Rutas para APIS FINAL ----------------------------------------------------------------
 
 app.listen(3000, (req, res) => {
     console.log('SERVER RUNNING IN http://localhost:3000');
